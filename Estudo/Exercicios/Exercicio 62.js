3c6232e9a4b47453d44b0acf2f89cf18baaf5a2c
Aerlon Gonçalves Alves <129308581+Aerlonga@users.noreply.github.com>
2023-12-05 16:56:58 -0300

    62

--- Estudo/Exercicios/Exercicio 62.js
+++ Estudo/Exercicios/Exercicio 62.js
@@ -1,4 +1,11 @@
 function fb (x){
-    return 
+    if (x % 3 === 0 && x % 5 === 0){return "FizzBuzz"};   
+    if (x % 3 === 0){return "Fizz"};
+    if (x % 5 === 0){return 'Buzz'};
+    if(typeof x !== "number"){return x}
+    return x
+};
 
+for (let i = 0; i <= 20; i++){
+    console.log(i, fb(i))
 };
