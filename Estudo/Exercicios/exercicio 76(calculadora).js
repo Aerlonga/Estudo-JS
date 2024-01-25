f2e94f50131f39061b482f2cd4284570b4154ecf
Aerlonga <aerlon.as@gmail.com>
2024-01-25 15:40:28 -0300

    att

--- Estudo/Exercicios/exercicio 76(calculadora).js
+++ Estudo/Exercicios/exercicio 76(calculadora).js
@@ -1,26 +1,31 @@
+// função que serve como "fábrica" para criar objetos da calculadora
 function criCalculadora() {
     return {
-        //atributos/variavéis dentro do objeto "criaCalculadora"
-
+        // Atributos/variáveis dentro do objeto "criaCalculadora"
         display: document.querySelector('.display'),
 
-
-
-        // metodos ou "ações"/"funcionalidades" especificas para o objeto "criaCalculadora" abaixo 
+        // Métodos ou "ações"/"funcionalidades" específicas para o objeto "criaCalculadora" abaixo 
         inicia(){
-
+            this.cliqueBotoes();
         },
 
         cliqueBotoes(){
+            // Adiciona um ouvinte de evento de clique ao documento
             document.addEventListener("click", function(e) {
+                // Obtém o elemento clicado
                 const el = e.target;
+
+                // Verifica se o elemento clicado possui a classe "btn-num"
                 if (el.classList.contains("btn-num")){
-                    btnParaDisplay();
+                    this.btnParaDisplay();
                 }
-            })
+            });
         },
     };
-};
+}
 
+// Cria uma instância da calculadora
 const calculadora = criCalculadora();
+
+// Inicia a calculadora chamando o método "inicia"
 calculadora.inicia();
